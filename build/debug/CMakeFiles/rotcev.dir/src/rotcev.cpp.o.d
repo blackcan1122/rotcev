1492b4f19651da46f69a81ee1efc57c2bf1dbec5
Blackcan <marcel.s@live.de>
2025-08-15 23:50:54 +0200

    changed git ignore

--- build/debug/CMakeFiles/rotcev.dir/src/rotcev.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/rotcev.dir/src/rotcev.cpp.o: \
- /home/blackcan/Documents/Hax0rStuff/Rotcev/src/rotcev.cpp \
- /usr/include/stdc-predef.h
--- build/release/CMakeFiles/rotcev.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rotcev.
-# This may be replaced when dependencies are built.
